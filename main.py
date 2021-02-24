8573da37e602df00f43068b8eb652d5991e0a5b5
Kenny <kenny_oliver@icloud.com>
2021-02-24 08:21:18 +0000

    [+] Fixed grammar for “There are 1 fishes!” [+] Add reminder for user of chosen coordinates before user is told if fish is present [+] Changing introduction strings [+] Changed row variable to x_guess [+] Changed column variable to y_guess

--- main.py
+++ main.py
@@ -11,28 +11,33 @@ from termcolor import colored
 import random
 #====================
 def find_fish(): 
-  print("All you have to do is guess where the fishes are!")
-  print("You score 10 points per fish\n") 
+  print("<-- Find the Fish -->")
+  print("Guess where the fishes are!")
+  print("10 points per fish\n") 
 
   score = 0 #initialise score variable to zero
   pond = [[0,0,0],[0,0,0],[0,0,0],[0,0,0]]
 
   repeat_range = random.randint(1,4)
-  print(f"There are {repeat_range} fishes!")
+  if repeat_range == 1:
+    print(f"There is {repeat_range} fish!")
+  else:
+    print(f"There are {repeat_range} fishes!")
   
   for _ in range(repeat_range):
     rand_x = random.randint(0,3)
     rand_y = random.randint(0,2)
     pond[rand_x][rand_y] = "F"
     
-    row = int(input("Guess row (0 to 3)\t--> "))
-    column = int(input("Guess column (0 to 2) --> "))
-    while row not in range(0,3+1):
-      row = int(input("ROW must be between 0-3\t\t--> "))
-    while column not in range(0,2+1):
-      column = int(input("COLUMN must be between 0-2 --> "))
+    x_guess = int(input("Guess row (0 to 3)\t--> "))
+    while x_guess not in range(0,3+1):
+      x_guess = int(input("ROW must be between 0-3\t\t--> "))
+    y_guess = int(input("Guess column (0 to 2) --> "))
+    while y_guess not in range(0,2+1):
+      y_guess = int(input("COLUMN must be between 0-2 --> "))
     
-    if pond[row][column] == "F":
+    print(f"Coords ({x_guess},{y_guess})")
+    if pond[x_guess][y_guess] == "F":
       print("Well done - you found a fish!\n")
       score += 10
     else:
