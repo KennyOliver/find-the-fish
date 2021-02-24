b4cecfc3b1d88b9853ffddf28c5dbfc9a5c70915
Kenny <kenny_oliver@icloud.com>
2021-02-24 07:56:27 +0000

    [+] Random number of multiple guesses [–] Remove from termcolor import coloured from main.py [+] Changing some strings [+] Fix syntax errors

--- main.py
+++ main.py
@@ -14,26 +14,24 @@ def find_fish():
   print("Welcome to the find the fish game - guess where the fish is!")
   print("You score 10 points if you find a fish\n") 
 
+  score = 0 #initialise score variable to zero
   pond = [[0,0,0],[0,0,0],[0,0,0],[0,0,0]] #set up the 2 D array
 
-  rand_x_first = random.randint(0,3)
-  rand_y_first = random.randint(0,2)
-  rand_x_second = random.randint(0,3)
-  rand_y_second = random.randint(0,2)
-
-  pond[rand_x_first][rand_y_first] = "F"
-  pond[rand_x_second][rand_y_second] = "F"
-
-  score = 0 #initialise score variable to zero
-
-  row = int(input("Guess the row number (0 to 3)\t--> "))
-  column = int(input("Guess the column number (0 to 2)\t--> "))
-
-  if pond[row][column] == "F":
-    print(colored("well done - you have found a fish!\n", 'green'))
-    score = score + 10
-  else:
-    print(" :) No fish here!\n")
+  repeat_range = random.randint(1,4)
+  
+  for _ in range(repeat_range):
+    rand_x = random.randint(0,3)
+    rand_y = random.randint(0,2)
+    pond[rand_x][rand_y] = "F"
+  
+    row = int(input("Guess the row number (0 to 3)\t--> "))
+    column = int(input("Guess the column number (0 to 2)\t--> "))
+  
+    if pond[row][column] == "F":
+      print("well done - you\'ve found a fish!\n")
+      score += 10
+    else:
+      print("No fish here!\n")
 
   print(f"Your score is {score}")
   print ("Thanks for playing - come back soon!")
